a6ce39ea653c06b72b39d231dad4c0bdda2b8995
tonyclem <tonyclem001@gmail.com>
2022-04-01 15:41:00 +0200

    rename some of the fils and added new lesson as well, callback

---
+++ callBackLessFiles/callback3.js
@@ -0,0 +1,55 @@
+// callBack function
+const callStr1 = false;
+const callStr2 = false;
+
+const callFunc1 = (callBack, callBackErr) => {
+  if (callStr1) {
+    callBack({
+      name: "clement",
+      age: "18",
+    });
+  } else if (callStr2) {
+    callBack({
+      address: "beijing",
+      phoneNumber: "123456789",
+    });
+  } else {
+    callBackErr("sorry, you are not allowed to access this page");
+  }
+};
+
+callFunc1(
+  (data) => {
+    console.log(data);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+// promise
+const callFunc2 = () => {
+  return new Promise((resolve, reject) => {
+    if (callStr1) {
+      resolve({
+        name: "clement",
+        age: "18",
+      });
+    } else if (callStr2) {
+      resolve({
+        address: "beijing",
+        phoneNumber: "123456789",
+      });
+    } else {
+      reject("sorry, you are not allowed to access this page");
+    }
+  });
+};
+
+callFunc2()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
